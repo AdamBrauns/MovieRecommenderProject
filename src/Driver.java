b18f4db32271c92240e9ee9889a6005cd269d447
Adam Brauns <BraunsAJ14@uww.edu>
2016-10-03 23:25:42 -0500

    Cleaned up countries output

--- src/Driver.java
+++ src/Driver.java
@@ -11,20 +11,20 @@ public class Driver {
 
     public static void main(String args[]){
         actors();
+        countries();
     }
 
     public static void actors(){
         File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_actors.txt");
         try {
             Scanner s = new Scanner(file);
-            PrintWriter writer = new PrintWriter("movie_actors_comma.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("src/movie_actors_comma.txt", "UTF-8");
             boolean skip = true;
             while(s.hasNextLine()){
                 String array[] = s.nextLine().split("\\t");
                 if(skip){
                     skip = false;
                 }else{
-                    //System.out.println(s.nextLine());
                     String movieID = array[0];
                     String actorID = array[1];
                     String actorName = array[2];
@@ -39,5 +39,32 @@ public class Driver {
             e.printStackTrace();
         }
     }
+
+    public static void countries(){
+        File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_countries.txt");
+        try {
+            Scanner s = new Scanner(file);
+            PrintWriter writer = new PrintWriter("src/movie_countries_comma.txt", "UTF-8");
+            boolean skip = true;
+            while(s.hasNextLine()){
+                String array[] = s.nextLine().split("\\t");
+                if(skip){
+                    skip = false;
+                }else{
+                    String movieID = array[0];
+                    System.out.println(movieID);
+                    String country = array[1];
+                    System.out.println(country);
+                    writer.println(movieID + ", " + country);
+                }
+            }
+            writer.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Something went wrong!");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
