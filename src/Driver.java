81edd2ad6fc525d318001e308f0caf9d8acd11d7
Adam Brauns <BraunsAJ14@uww.edu>
2016-10-04 16:23:43 -0500

    cleaned up output and made into csv

--- src/Driver.java
+++ src/Driver.java
@@ -10,17 +10,18 @@ import java.io.PrintWriter;
 public class Driver {
 
     public static void main(String args[]){
-        //actors();
-        //countries();
-        //directors();
+        actors();
+        countries();
+        directors();
         genres();
+        movies();
     }
 
     public static void actors(){
         File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_actors.txt");
         try {
             Scanner s = new Scanner(file);
-            PrintWriter writer = new PrintWriter("src/movie_actors_comma.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("src/movie_actors_comma.csv", "UTF-8");
             boolean skip = true;
             while(s.hasNextLine()){
                 String array[] = s.nextLine().split("\\t");
@@ -46,7 +47,7 @@ public class Driver {
         File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_countries.txt");
         try {
             Scanner s = new Scanner(file);
-            PrintWriter writer = new PrintWriter("src/movie_countries_comma.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("src/movie_countries_comma.csv", "UTF-8");
             boolean skip = true;
             while(s.hasNextLine()){
                 String array[] = s.nextLine().split("\\t");
@@ -69,7 +70,7 @@ public class Driver {
         File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_directors.txt");
         try {
             Scanner s = new Scanner(file);
-            PrintWriter writer = new PrintWriter("src/movie_directors_comma.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("src/movie_directors_comma.csv", "UTF-8");
             boolean skip = true;
             while(s.hasNextLine()){
                 String array[] = s.nextLine().split("\\t");
@@ -93,7 +94,7 @@ public class Driver {
         File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_genres.txt");
         try {
             Scanner s = new Scanner(file);
-            PrintWriter writer = new PrintWriter("src/movie_genres_comma.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("src/movie_genres_comma.csv", "UTF-8");
             boolean skip = true;
             while(s.hasNextLine()){
                 String array[] = s.nextLine().split("\\t");
@@ -101,11 +102,53 @@ public class Driver {
                     skip = false;
                 }else{
                     String movieID = array[0];
-                    System.out.println(movieID);
                     String genre = array[1];
-                    System.out.println(genre);
                     writer.println(movieID + "," + genre);
                 }
+            }
+            writer.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Something went wrong!");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void movies(){
+        File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie2.txt");
+        try {
+            Scanner s = new Scanner(file);
+            PrintWriter writer = new PrintWriter("src/movies_comma.csv", "UTF-8");
+            while(s.hasNextLine()){
+                String array[] = s.nextLine().split("\\t");
+                String id = array[0];
+                String title = array[1];
+                String imdbID = array[2];
+                String spanishTitle = array[3];
+                String imdbPictureURL = array[4];
+                String year = array[5];
+                String rtID = array[6];
+                String rtAllCriticsRating = array[7];
+                String rtAllCriticsNumReviews = array[8];
+                String rtAllCriticsNumFresh = array[1];
+                String rtAllCriticsNumRotten = array[2];
+                String rtAllcriticsScore = array[1];
+                String rtTopCriticsRating = array[2];
+                String rtTopCriticsNumReviews = array[1];
+                String rtTopCriticsNumFresh = array[2];
+                String rtTopCriticsNumRotten = array[1];
+                String rtTopCriticsScore = array[2];
+                String rtAudienceRating = array[1];
+                String rtAudienceNumRatings = array[2];
+                String rtAudienceScore = array[1];
+                String rtPictureURL = array[2];
+                //int rating = Integer.parseInt(array[3]);
+                writer.println(id + ","+title+","+imdbID+","+spanishTitle+","+imdbPictureURL+","+
+                                year+","+rtID+","+rtAllCriticsRating+","+rtAllCriticsNumReviews+","+
+                                rtAllCriticsNumFresh+","+rtAllCriticsNumRotten+","+rtAllcriticsScore+","+
+                                rtTopCriticsRating+","+rtTopCriticsNumReviews+","+rtTopCriticsNumFresh+","+
+                                rtTopCriticsNumRotten+","+rtTopCriticsScore+","+rtAudienceRating+","+
+                                rtAudienceNumRatings+","+rtAudienceScore+","+rtPictureURL);
+
             }
             writer.close();
         }catch(FileNotFoundException e){
