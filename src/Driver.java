63c6875f4a51d7c4b53423777f662dc7c15d131d
Adam Brauns <BraunsAJ14@uww.edu>
2016-10-04 15:22:22 -0500

    added directors and genres

--- src/Driver.java
+++ src/Driver.java
@@ -10,8 +10,10 @@ import java.io.PrintWriter;
 public class Driver {
 
     public static void main(String args[]){
-        actors();
-        countries();
+        //actors();
+        //countries();
+        //directors();
+        genres();
     }
 
     public static void actors(){
@@ -29,7 +31,7 @@ public class Driver {
                     String actorID = array[1];
                     String actorName = array[2];
                     int rating = Integer.parseInt(array[3]);
-                    writer.println(movieID + ", " + actorID + ", " + actorName + ", " + rating);
+                    writer.println(movieID + "," + actorID + "," + actorName + "," + rating);
                 }
             }
             writer.close();
@@ -52,10 +54,57 @@ public class Driver {
                     skip = false;
                 }else{
                     String movieID = array[0];
-                    System.out.println(movieID);
                     String country = array[1];
-                    System.out.println(country);
-                    writer.println(movieID + ", " + country);
+                    writer.println(movieID + "," + country);
+                }
+            }
+            writer.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Something went wrong!");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void directors(){
+        File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_directors.txt");
+        try {
+            Scanner s = new Scanner(file);
+            PrintWriter writer = new PrintWriter("src/movie_directors_comma.txt", "UTF-8");
+            boolean skip = true;
+            while(s.hasNextLine()){
+                String array[] = s.nextLine().split("\\t");
+                if(skip){
+                    skip = false;
+                }else{
+                    String movieID = array[0];
+                    String directorID = array[1];
+                    String directorName = array[2];
+                    writer.println(movieID+ "," +directorID+","+directorName);
+                }
+            }
+            writer.close();
+        }catch(FileNotFoundException e){
+            System.out.println("Something went wrong!");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void genres(){
+        File file = new File("/Users/AdamBrauns/IdeaProjects/MovieRecommender/src/movie_genres.txt");
+        try {
+            Scanner s = new Scanner(file);
+            PrintWriter writer = new PrintWriter("src/movie_genres_comma.txt", "UTF-8");
+            boolean skip = true;
+            while(s.hasNextLine()){
+                String array[] = s.nextLine().split("\\t");
+                if(skip){
+                    skip = false;
+                }else{
+                    String movieID = array[0];
+                    System.out.println(movieID);
+                    String genre = array[1];
+                    System.out.println(genre);
+                    writer.println(movieID + "," + genre);
                 }
             }
             writer.close();
