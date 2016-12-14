5aed9a34068fa8bc04083731f4c2af16efed1f0a
Adam Brauns <BraunsAJ14@uww.edu>
2016-12-14 17:58:26 -0600

    cleaned up files and removed some javascript files

--- src/view/htmlphp/movieInfo.php
+++ src/view/htmlphp/movieInfo.php
@@ -16,15 +16,7 @@ if($_SESSION['active'] == false){
 <link rel="stylesheet" href="../css/reset.css" type="text/css" media="all">
 <link rel="stylesheet" href="../css/layout.css" type="text/css" media="all">
 <link rel="stylesheet" href="../css/testcss.css" type="text/css" media="all">
-<!--
-<link rel="stylesheet" href="../css/style.css" type="text/css" media="all">
-<script type="text/javascript" src="../js/jquery-1.4.2.js" ></script>
-<script type="text/javascript" src="../js/cufon-yui.js"></script>
-<script type="text/javascript" src="../js/cufon-replace.js"></script>
-<script type="text/javascript" src="../js/Myriad_Pro_italic_600.font.js"></script>
-<script type="text/javascript" src="../js/Myriad_Pro_italic_400.font.js"></script>
-<script type="text/javascript" src="../js/Myriad_Pro_400.font.js"></script>
--->
+
 </head>
 <body>
 <div class="body1">
